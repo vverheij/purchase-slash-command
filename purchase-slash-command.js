20c29b9271e060f59e2f9f39be5e2feb9ad86a53
Victor Verheij <v.verheij@gmail.com>
2021-03-22 14:05:22 +0100

    'working_exmple'

--- purchase-slash-command.js
+++ purchase-slash-command.js
@@ -1,16 +1,30 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // allow us to easily process POST requests
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(urlencoded({ extended: false }));
+// app.use(json());
 
-require('./routes/purchase')(app);
-require('./routes/action')(app);
+// require('./routes/purchase')(app);
+// require('./routes/action')(app);
 
-const PORT = 9647;
+app.post('/purchase', async (req, res) => {
+  console.log(req)
+  const { text, user_id } = req.body;
+  res.json({
+    text: `Thanks for your purchase request of *${text}*. We will message the CEO now for authorisation.`
+  })
+// res.send({ status: 'succes'})
+res.send();
+})
+
+// const PORT = 9647;
+const PORT = 9467;
 app.listen(PORT, () => {
   console.log(`Slack bot server has started on port ${PORT}`);
-});
+})
+
